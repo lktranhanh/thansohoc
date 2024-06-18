221c6c43079c71aff02f4b929e3ba4dc685a4782
hanh.tran <39853213+lktranhanh@users.noreply.github.com>
2024-06-18 14:33:08 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // import Head from "next/head";
 import React, { ReactNode, useEffect } from 'react';
+import Head from 'next/head';
 import { ChakraProvider } from "@chakra-ui/react";
 import Script from "next/script";
 import { theme } from "Utils/themes";
@@ -14,12 +15,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                name="description"
                content="Tra Cứu Numerology TheLotusCoach"
             />
-            <meta 
-               name="robots" 
-               content="index, follow">
-             <meta 
-                name="google-adsense-account" 
-                content="ca-pub-8703671366589995">  
+             <Head>
+            <meta name="robots" content="index, follow" />
+            <meta name="google-adsense-account" content="ca-pub-8703671366589995" />
+            </Head>
             <link rel="icon" href="/Images/logoPNG.png" />
             <Script 
                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8703671366589995" 
