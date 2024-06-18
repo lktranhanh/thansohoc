7e0955de2a7fb939cfb7f73f02d6baf54dec73d6
hanh.tran <39853213+lktranhanh@users.noreply.github.com>
2024-06-18 14:05:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                content="Tra Cứu Numerology TheLotusCoach"
             />
             <link rel="icon" href="/Images/logoPNG.png" />
-            <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8703671366589995" crossorigin="anonymous" />
+            <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8703671366589995" crossOrigin="anonymous" />
          
          </head>
 
